379fc9ed84d9ea1c91c0c9c4ef85c026b01eac74
Danish Ranjan <ranjandanish@gmail.com>
2020-12-28 19:04:46 +0530

    Print Decreasing

---
+++ printDecreasing.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class printDecreasing {
+public static void main(String args[]) {
+	Scanner sc = new Scanner(System.in);
+	int n = sc.nextInt();
+	decreasing(n);
+}
+public static void decreasing(int n){
+	if(n==0) {
+		return;
+	}
+	System.out.println(n);
+	decreasing(n-1);
+}
+}
